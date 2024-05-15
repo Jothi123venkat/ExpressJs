42ae44794e8c9e97be50f8c66ae3f4ca53651aed
venkat <erjothivenkat2002@gmail.com>
2024-05-15 13:53:44 +0530

    nodemon and tyepe module setup

--- Web_server.js
+++ Web_server.js
@@ -1,9 +1,18 @@
-import React from 'react'
 
-const Web_server = () => {
-  return (
-    <div>Web_server</div>
-  )
-}
 
-export default Web_server
+import express from 'express';
+
+
+const app = express();
+
+
+const  port = 5000;
+
+app.get("/",(req,res)=>{
+    res.json({name:"hello world",age:"25"});
+})
+
+app.listen(port,(()=>{
+    console.log(`The port is running ${port}`);
+
+}))
